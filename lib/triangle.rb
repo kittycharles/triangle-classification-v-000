71ea85a200b3cf5c3cb308fae23d4b86d50c30cc
Richard Sanchez <richard.charles.s@gmail.com>
2018-08-14 21:49:11 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -9,9 +9,9 @@ class Triangle
 
   def valid_triangle?
   (@a+@b > @c).
-  && (@b+@c > @a).
-  && (@a+@c > @b).
-  && @a>0 && @b>0 && @c>0
+  &&(@b+@c > @a).
+  &&(@a+@c > @b).
+  &&@a>0 && @b>0 && @c>0
   end
 
   def kind
